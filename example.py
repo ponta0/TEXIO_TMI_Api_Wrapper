eadbe8c882148ad4e7a90be7fa2a3ef1b17a48a1
まぐにあ <dev@mail.mag-neko.net>
2022-11-06 16:49:19 +0900

    解説の追加

--- example.py
+++ example.py
@@ -7,23 +7,26 @@ delay = 1
 
 print("電源に接続します")
 dev_hID = api.TMI_HandleOpen("PW-A", "USB:1:1")
-print(f"hID: {dev_hID}")
 time.sleep(delay)
 
 if dev_hID > 0:
-    #print(f"hID: {dev_hID}")
+    print(f"hID: {dev_hID}")
+
+    print("機種名を表示します")
     ret, name = api.TMI_ModelNameQ(dev_hID)
     print(f"ret: {ret}")
     print(f"Model: {name}")
     time.sleep(delay)
 
+    print("電圧, 電流設定値を表示します")
     ret, data = api.TMI_AllPresetQ(dev_hID)
+    print(f"ret: {ret}")
     print(list(data))
-
     time.sleep(delay)
+
+    print("通信を切断します")
     ret = api.TMI_HandleClose(dev_hID)
     print(f"ret: {ret}")
     print("電源との通信を切断しました")
 else:
     print(f"エラーコード: {dev_hID}")
-
